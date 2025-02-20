902a518cf6aae795afa37cc980a88dfedcf405df
Vadym <67538058+Extractoro@users.noreply.github.com>
2025-02-20 15:52:18 +0200

    perf: delete empty file

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
 import { CountriesModule } from './countries/countries.module';
 import {ConfigModule} from "@nestjs/config";
 
 @Module({
   imports: [ConfigModule.forRoot({ isGlobal: true }), CountriesModule],
-  controllers: [AppController],
 })
 export class AppModule {}
